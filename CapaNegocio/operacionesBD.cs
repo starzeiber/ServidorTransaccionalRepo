7af0cc51f2e31901a3311ddad1313d12e4678f7f
starzeiber <44450752+starzeiber@users.noreply.github.com>
2019-06-13 11:29:42 -0500

    migracion

---
+++ CapaNegocio/operacionesBD.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaNegocio
+{
+    public static class operacionesBD
+    {
+        public static List<String> Obtener(String algo)
+        {
+            ResultadoBaseDatos resultados = new ResultadoBaseDatos();
+            List<String> lista = new List<String>();
+            try
+            {
+                List<SqlParameter> parametros = new List<SqlParameter>()
+                {
+                    new SqlParameter("@","")
+                };
+
+                //111116 se ejecuta el SP
+                resultados = OperacionesBaseDatos.EjecutaSP("SPR", parametros);
+
+                //111116 se revisa que existe un error
+                if (!resultados.Error)
+                {
+                    //111116 se revisa que existan resultados
+                    if (resultados.Datos.Tables.Count > 0)
+                    {
+                        //111116 se recorren los resultados
+                        if (resultados.Datos.Tables[0].Rows.Count > 0)
+                        {
+                            foreach (DataRow dr in resultados.Datos.Tables[0].Rows)
+                            {
+                                //Area area = new Area();
+                                //area.idArea = dr.Field<int>("idArea");
+                                //area.nombreArea = dr.Field<String>("nombreArea");
+                                //listaAreas.Add(area);
+                            }
+                            //cLogErrores.Escribir_Log_Advertencia("Se realizó la inserción correctamente");
+                        }
+                        else
+                        {                            
+                            //cLogErrores.Escribir_Log_Advertencia("no hay resultados, GraficaServidoresNegocio,Operaciones,ObtenerAreasGrafica");
+                        }
+                    }
+                    else
+                    {                        
+                        //cLogErrores.Escribir_Log_Advertencia("no hay resultados, GraficaServidoresNegocio,Operaciones,ObtenerAreasGrafica");
+                    }
+                }
+                else
+                {                    
+                    //cLogErrores.Escribir_Log_Error(resultados.Excepcion.Message + " GraficaServidoresNegocio,Operaciones,ObtenerAreasGrafica");
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {                
+                //cLogErrores.Escribir_Log_Error("GraficaServidoresNegocio,Operaciones,ObtenerAreasGrafica: " + ex.Message);
+                return lista;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
---
+++ ServidorCore/estadoSocketDeTrabajo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServidorCore
+{
+    /// <summary>
+    /// Clase que contiene las funciones que se utilizan para indicar el flujo de una operación con el cliente
+    /// </summary>
+    public class estadoSocketDeTrabajo
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public estadoSocketDeTrabajo() { }
+
+        // Referencia al proceso principal donde se encuentra el socket principal que disparó el flujo
+        public object procesoPrincipal { get; set; }
+
+        /// <summary>
+        /// Función virtual para sobre escribirla que se utiliza cuando se requiera un mensaje de
+        /// bienvenida a una conexión de un cliente
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public virtual string mensajeBienvenida(object args)
+        {
+            return "";
+        }
+
+        /// <summary>
+        /// función virtual para sobre escribirla que se utiliza para indicar el principio del flujo
+        /// </summary>
+        public virtual void OnInicio()
+        {
+        }
+
+        /// <summary>
+        /// función virtual para sobre escribirla que se utiliza para indicar en el flujo que un cliente se cierra
+        /// </summary>
+        /// <param name="args"></param>
+        public virtual void OnClienteCerrado(object args)
+        {
+        }
+
+        /// <summary>
+        /// función virtual para sobre escribirla que se utiliza para indicar en el flujo que hay una conexión
+        /// </summary>
+        public virtual void OnConexion()
+        {
+        }
+
+        /// <summary>
+        /// función virtual para sobre escribirla que se utiliza indicar en el flujo que se acepta una solicitud de mensaje
+        /// </summary>
+        /// <param name="args"></param>
+        public virtual void OnAceptacion(object args)
+        {
+        }
+
+        /// <summary>
+        /// función virtual para sobre escribirla que se utiliza indicar en el flujo que se recibe un mensaje
+        /// </summary>
+        public virtual void OnRecibido()
+        {
+        }
+
+        /// <summary>
+        /// función virtual para sobre escribirla que se utiliza indicar en el flujo que se envía un mensaje
+        /// </summary>
+        public virtual void OnEnviado()
+        {
+        }
+    }
+}
