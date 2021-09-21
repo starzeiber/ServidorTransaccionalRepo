96aa0f7d79eb43fe283d84205f3deac368ef04e6
starzeiber <44450752+starzeiber@users.noreply.github.com>
2021-09-21 11:05:52 -0500

    Actualización
    Actualización

--- CapaNegocio/SP/sprInsertarVenta.sql
+++ CapaNegocio/SP/sprInsertarVenta.sql
@@ -20,15 +20,15 @@ GO
 -- =============================================
 ALTER PROCEDURE sprInsertarVenta
 	@fechaHora datetime,
-	@fecha varchar(10),
-	@hora varchar(10),
+	@fecha varchar(20),
+	@hora varchar(20),
 	@encabezado int,
 	@pCode int,
-	@issuer varchar(15),
-	@sku varchar(20),
+	@issuer varchar(10),
+	@sku varchar(30),
 	@folio varchar(50),
-	@numeroReferencia varchar(12),
-	@telefono varchar(15),
+	@numeroReferencia varchar(20),
+	@telefono varchar(20),
 	@monto decimal,	
 	@idGrupo int,
 	@idCadena int,
@@ -47,7 +47,6 @@ ALTER PROCEDURE sprInsertarVenta
 AS
 BEGIN
 	
-
 	INSERT INTO TransactionSales
 		(DATE_TIME,LOCAL_DATE,LOCAL_TIME,MSG_TYPE,PCODE,ISSUER,SKU,FOLIO,REFERENCE_NUMBER,NRO_TELEFONICO,AMOUNT,ID_GROUP,ID_CHAIN,ID_MERCHANT,ID_POS,ID_TELLER,ID_PROVIDER,ID_MASTER,SYSTEMTRACE,[AUTHORIZATION],RESPONSE_CODE,SALDOACTUAL,STATUS,STATUSPLAT,MSG200,MSG210,TypeTRX) values
 		(@fechaHora,@fecha,@hora,@encabezado,@pCode,@issuer,@sku,@folio,@numeroReferencia,@telefono,@monto,@idGrupo,@idCadena,@idTienda,1,0,@idProveedor,@idMaster,@numTransaccion,@autorizacion,@codigoRespuesta,@saldoActual,1,0,@mensaje200,@mensaje210,@tipoTrx)
