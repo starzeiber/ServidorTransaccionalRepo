4f86509f0974a89dc430a6d123bf7c3b5209eb6e
Uriel Mtz <44450752+starzeiber@users.noreply.github.com>
2023-06-29 16:10:44 -0600

    cambios de rama

--- ServidorCore/Properties/AssemblyInfo.cs
+++ ServidorCore/Properties/AssemblyInfo.cs
@@ -65,8 +65,7 @@ using System.Runtime.InteropServices;
 //@UMB      16/03/23    9.4.0.0     Factorización de la capa de seguridad y logTrace. Proyecto funcional y completo, se puede factorizar más pero ya no es necesario
 //@UMB      06/05/23    9.4.1.0     Se realizan más especificos los comentarios en algunas propiedades
 //@UMB      05/06/13    9.5.0.0     Se quitan los códigos de respuesta de TAE, solo se deban los importantes del proceso CORE
+//@UMB      12/06/13    10.0.0.0    Se  refactorizan los  nombres  al idioma inglés  para  hacerlo más genérico
 
-
-
-[assembly: AssemblyVersion("9.5.0.0")]
-[assembly: AssemblyFileVersion("9.5.0.0")]
+[assembly: AssemblyVersion("10.0.0.0")]
+[assembly: AssemblyFileVersion("10.0.0.0")]
